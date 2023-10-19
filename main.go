9545ead541d9e77fc0cc19c06f9eb2b351c176b4
阳明 <ych_1024@163.com>
2023-10-19 16:37:26 +0800

    版本更新&添加新数据

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-const VERSION = "0.1.8"
+const VERSION = "0.2.0"
 
 func main() {
 	pwd := os.Getenv("DB_PASSWORD")
@@ -75,7 +75,7 @@ func InsertInitData(db *sql.DB) {
 	}
 	defer stmt.Close()
 
-	_, err = stmt.Exec("KubeVela", "It's a test user")
+	_, err = stmt.Exec("KubeVela2", "It's another test user")
 	if err != nil {
 		panic(err)
 	}
